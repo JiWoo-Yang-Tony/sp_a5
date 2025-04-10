b78f2b24f51a53dcc7220b96980bef91a86c8e9b
Jonathan Paventi <jpaventi7582@conestogac.on.ca>
2025-04-10 14:45:33 -0400

    Created structure for the DC. It currently uses some volatile variables, which I'm not sure is the best way to do this, but it works for now. There are some things missing like dealing with all of the variables that DP2 passes to the DC and properly implementing the kill() of DP1 and 2. I'm not sure how to properly get the PID's of those things.

--- HISTO-SYSTEM/inc/consumer/dc.h
+++ HISTO-SYSTEM/inc/consumer/dc.h
@@ -1,5 +1,5 @@
 // FILE NAME    : dc.h
-// PROGRAMMER   : Josh Horsley, Kalina Cathcart, John Paventi, Daimon Quin, Jiwoo Yang
+// PROGRAMMER   : Josh Horsley, Kalina Cathcart, Jon Paventi, Daimon Quin, Jiwoo Yang
 // DATE         : 2025-4-10
 // DESCRIPTION  : 
 
@@ -9,7 +9,8 @@
 #include "../../inc/shared.h"
 
 // Function declarations for DC process
+void sigalarm_handler(int signal);
 
-
+void sigint_handler(int signal);
 
 #endif
